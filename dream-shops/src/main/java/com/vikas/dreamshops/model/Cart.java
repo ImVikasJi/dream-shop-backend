0d98c09fd1b3e8c38a62c8d2e248edd6aab37553
Admin <Admin@DESKTOP-59D9DUP>
2024-10-05 15:24:54 +0530

    Cart and CartItem Entity

---
+++ dream-shops/src/main/java/com/vikas/dreamshops/model/Cart.java
@@ -0,0 +1,33 @@
+package com.vikas.dreamshops.model;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal totalAmount = BigDecimal.ZERO;
+	
+	@OneToMany(mappedBy = "cart",cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<CartItem> cartItems;
+	
+}
